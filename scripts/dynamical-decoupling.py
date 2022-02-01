866f6d15f568abdc51b9bcfc89ab9b91ed69a73c
Ben <bjbraham@gmail.com>
2022-02-01 22:32:28 +1100

    Update dynamical-decoupling script

--- scripts/dynamical-decoupling.py
+++ scripts/dynamical-decoupling.py
@@ -1,5 +1,5 @@
 import numpy as np
-from qutip import Qobj, tensor
+from qutip import Qobj
 from qutip_qip.operations import z_gate, x_gate
 from qutip_qip.vqa import VQA, VQA_Block
 
@@ -9,17 +9,35 @@ H = Qobj([
     ])
 
 k = 1
+
 VQA_circuit = VQA(
-            n_qubits = 1,
-            n_layers = k,
-            cost_method = "OBSERVABLE",
+            n_qubits=1,
+            n_layers=4*k,
+            cost_method="STATE",
         )
 
-U_block = VQA_Block(1/(2*k) * H, name="U(t)")
-U_block = VQA_Block(Qobj([[0, 1],[1, 0]]),name="X(t)")
+
+def get_layer_unitary(t):
+    U = (-1j * H * t/(8*k)).expm()
+    return (z_gate() * U * x_gate() * U)
+
+
+# overlap of initial state with final state
+def state_overlap_cost(state):
+    initial_state = VQA_circuit.get_initial_state()
+    overlap_prob = abs(state.overlap(initial_state))**2
+    return 1 - overlap_prob
+
+
+VQA_circuit.cost_func = state_overlap_cost
+
+U_block = VQA_Block(get_layer_unitary, is_unitary=True, name="U(t)")
 VQA_circuit.add_block(U_block)
-VQA_circuit.add_block(VQA_Block("x_gate", targets=[0]))
-VQA_circuit.repeat_block(U_block)
-VQA_circuit.add_block(VQA_Block("z_gate", targets=[0]))
-VQA_circuit.export_image()
-print(VQA_circuit.get_final_state([0.5]))
+
+res = VQA_circuit.optimize_parameters(
+        method='Powell',
+        initial='random',
+        bounds=[(0, np.inf) for _ in range(4*k)]
+        )
+print(res)
+print('Total time', sum(res.angles))
