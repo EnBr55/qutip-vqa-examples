4ff8d50378ecf6c6e52bc679e630f378c7de16e5
Ben <bjbraham@gmail.com>
2022-02-02 14:05:59 +1100

    Add constraints to dynamical decoupling example

--- scripts/dynamical-decoupling.py
+++ scripts/dynamical-decoupling.py
@@ -1,8 +1,14 @@
 import numpy as np
-from qutip import Qobj
+from qutip import Qobj, rand_herm, qeye
 from qutip_qip.operations import z_gate, x_gate
 from qutip_qip.vqa import VQA, VQA_Block
+from scipy.optimize import LinearConstraint
+from qutip.qip.operations.gates import gate_sequence_product
 
+#H = rand_herm(N=2)
+#H = H - H.tr()/2 * qeye(2)
+
+# Traceless, Hermitian operator
 H = Qobj([
     [1, 1],
     [1, -1]
@@ -16,6 +22,7 @@ VQA_circuit = VQA(
             cost_method="STATE",
         )
 
+# One 'block' is given by (Z e^{-iH t/8k} X e^{-iH t/8k})^{4k}
 
 def get_layer_unitary(t):
     U = (-1j * H * t/(8*k)).expm()
@@ -34,10 +41,32 @@ VQA_circuit.cost_func = state_overlap_cost
 U_block = VQA_Block(get_layer_unitary, is_unitary=True, name="U(t)")
 VQA_circuit.add_block(U_block)
 
+# Set lower and upper bounds to get the time as close to 1 as possible
+
+def constraint_lb(t):
+    return sum(t) - 0.999
+
+def constraint_ub(t):
+    return 1.001 - sum(t)
+
 res = VQA_circuit.optimize_parameters(
-        method='Powell',
+        method='COBYLA',
         initial='random',
-        bounds=[(0, np.inf) for _ in range(4*k)]
+        constraints=[
+            {
+            'type': 'ineq',
+            'fun': constraint_lb
+            },
+            {
+            'type': 'ineq',
+            'fun': constraint_ub
+            },
+            ],
         )
 print(res)
 print('Total time', sum(res.angles))
+print(
+    'Total unitary',
+    gate_sequence_product(
+        VQA_circuit.construct_circuit(res.res.x).propagators())
+    )
